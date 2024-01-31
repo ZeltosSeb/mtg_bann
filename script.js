5e5c723a93afd0325678824566cd138ff0f630fb
SebastianSurwehme <431381@student.saxion.nl>
2024-01-31 18:50:38 +0100

    Dynamic select options from json

--- script.js
+++ script.js
@@ -5,22 +5,48 @@ document.addEventListener("DOMContentLoaded", function () {
   let addToClipboardDeckBtn = document.getElementById("toClipboard_clean");
   let addToClipboardBanBtn = document.getElementById("toClipboard_dirty");
   let userInput = document.getElementById("userListInput");
-  let deckName = document.getElementById("inputDeckName");
+  let inputDeckName = document.getElementById("inputDeckName");
+  let deckSelector = document.getElementById("deckSelector");
+  let banSelector = document.getElementById("banSelector");
 
   let deckJSON = fetch("deck.json").then(response => response.json()).then(data => {
-    console.log("Json Deck geladen.")
-  })
-    .catch(error => console.error('Fehler beim Laden der JSON-Datei:', error));
+    console.log("Json Deck geladen.");
+    addToSelector(data);
+
+  }).catch(error => console.error('Fehler beim Laden der JSON-Datei:', error))
+
 
   let banJSON = fetch("ban.json").then(response => response.json()).then(data => {
     console.log("Json Ban geladen.")
 
+    addToSelector(data);
+
   })
     .catch(error => console.error('Fehler beim Laden der JSON-Datei:', error));
 
 
+  function addToSelector(list) {
 
+    let collectionTitel;
+    let i = 0;
 
+    if (list.decks) {
+      collectionTitel = list.decks[i].deckName;
+
+      for (i = 0; i < list.decks.length; i++) {
+        let htmlTxt = `<option value="${collectionTitel}">${collectionTitel}</option>`;
+        deckSelector.innerHTML += htmlTxt;
+      }
+
+    }
+    else if (list.banLists) {
+      collectionTitel = list.banLists[i].banListName;
+      for (i = 0; i < list.banLists.length; i++) {
+        let htmlTxt = `<option value="${collectionTitel}">${collectionTitel}</option>`;
+        banSelector.innerHTML += htmlTxt;
+      }
+    }
+  }
 
   addToDeckBtn.addEventListener("click", function () {
 
