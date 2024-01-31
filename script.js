01d073f5ff7a87c9a42a415bfeb2be822edadccb
SebastianSurwehme <431381@student.saxion.nl>
2024-01-31 17:42:52 +0100

    addedEventListener to buttons

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
-  //   let addToDeckBtn = document.getElementById("AddToDeckList");
-  //   let addToBannListBtn = document.getElementById("AddToBannList");
+  let addToDeckBtn = document.getElementById("AddToDeckList");
+  let addToBanListBtn = document.getElementById("AddToBannList");
   let clearTextBtn = document.getElementById("clearTextarea");
-  //   let addToClipboardDeckBtn = document.getElementById("toClipboard_clean");
-  //   let addToClipboardBannBtn = document.getElementById("toClipboard_dirty");
+  let addToClipboardDeckBtn = document.getElementById("toClipboard_clean");
+  let addToClipboardBanBtn = document.getElementById("toClipboard_dirty");
   let userInput = document.getElementById("userListInput");
+  let deckName = document.getElementById("inputDeckName");
 
-  function addToDeckList() {
-    console.log("TEST");
-  }
+  let deckJSON = fetch("deck.json").then(response => response.json()).then(data => {
+    console.log("Json Deck geladen.")
+  })
+    .catch(error => console.error('Fehler beim Laden der JSON-Datei:', error));
 
-  function addToBannList() {
-    console.log("TEST");
-  }
+  let banJSON = fetch("ban.json").then(response => response.json()).then(data => {
+    console.log("Json Ban geladen.")
 
-  function clearText() {
-    userInput.value = "";
-  }
+  })
+    .catch(error => console.error('Fehler beim Laden der JSON-Datei:', error));
+
+
+
+
+
+  addToDeckBtn.addEventListener("click", function () {
+
+  });
+  addToBanListBtn.addEventListener("click", function () {
+
+  });
 
   clearTextBtn.addEventListener("click", function () {
-    // Call your function or execute your code here
     clearText();
   });
+
+  addToClipboardDeckBtn.addEventListener("click", () => {
+  })
+
+  addToClipboardBanBtn.addEventListener("click", () => {
+  })
+
 
   function toClipboard(status) {
     if (status == "clean") {
@@ -30,4 +47,10 @@ document.addEventListener("DOMContentLoaded", function () {
       console.log("dirty");
     }
   }
+
+  function clearText() {
+    userInput.value = "";
+  }
+
 });
+
